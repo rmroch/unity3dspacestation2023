6db8b21c28d49e470c7ea8c25dec58275cef65c2
Robert Mroch <robmroch@yahoo.com>
2023-04-27 12:55:03 -0500

    add flame to missles

--- Assets/Scripts/MissleControl.cs
+++ Assets/Scripts/MissleControl.cs
@@ -24,28 +24,29 @@ public class MissleControl : MonoBehaviour
              if(Input.GetMouseButtonDown(0)) {
                 switch(hit.collider.gameObject.tag) {
                     case "Player1":
-                        Debug.Log("Player 1 has been clicked");
+                        //Debug.Log("Player 1 has been clicked");
                         GameObject[] player1GameObject = GameObject.FindGameObjectsWithTag("Player1");
                         if(player1GameObject.Length > 0) {
+                            player1GameObject[0].transform.GetChild(0).gameObject.GetComponent<Renderer>().enabled = false;
                             player1GameObject[0].GetComponent<Rigidbody>().AddForce(0f, 0f, m_Thrust, ForceMode.Acceleration);
                         }
                         break;
                     case "Player2":
-                        Debug.Log("Player 2 has been clicked");
+                        //Debug.Log("Player 2 has been clicked");
                         GameObject[] player2GameObject = GameObject.FindGameObjectsWithTag("Player2");
                         if(player2GameObject.Length > 0) {
                             player2GameObject[0].GetComponent<Rigidbody>().AddForce(m_Thrust, 0f, 0f, ForceMode.Acceleration);
                         }
                         break;
                     case "Player3":
-                        Debug.Log("Player 3 has been clicked");
+                        //Debug.Log("Player 3 has been clicked");
                         GameObject[] player3GameObject = GameObject.FindGameObjectsWithTag("Player3");
                         if(player3GameObject.Length > 0) {
                             player3GameObject[0].GetComponent<Rigidbody>().AddForce(0f, 0f, -m_Thrust, ForceMode.Acceleration);
                         }
                         break;
                     case "Player4":
-                        Debug.Log("Player 4 has been clicked");
+                        //Debug.Log("Player 4 has been clicked");
                         GameObject[] player4GameObject = GameObject.FindGameObjectsWithTag("Player4");
                         if(player4GameObject.Length > 0) {
                             player4GameObject[0].GetComponent<Rigidbody>().AddForce(-m_Thrust, 0f, 0f, ForceMode.Acceleration);
